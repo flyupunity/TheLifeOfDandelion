cdd8cba04a19e03db27a7741a0294a435fb8ca17
flyupunity <flyupworking@gmail.com>
2024-06-09 13:58:41 +0300

    Edit
    without "Dandelion_Game\Screenshots\Life_of_Dandelion 2024-03-16 17-09-19.mp4"

--- Dandelion_Game/Assets/Scripts/GamePlay_3/GP_3_BeeCreator.cs
+++ Dandelion_Game/Assets/Scripts/GamePlay_3/GP_3_BeeCreator.cs
@@ -1,21 +1,41 @@
+﻿using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class GP_3_BeeCreator : MonoBehaviour
 {
-	public Transform PlayerT;
+
 	public GameObject Bee1;
 	public Transform Point;
 
-	public int i = 1;
-	public float Timer1;
-	public float MaxTimer1;
+	[Space(15)]
 
-	void FixedUpdate()
+
+    #region Time
+
+	[Header("Time")][Space(10)]
+    public float Timer = 0;
+	public float MaxTimer = 30;
+    [Range(0f, 15f)] public float Scatter = 5;
+
+    #endregion
+
+    private void Awake()
+    {
+		StartCoroutine(RandomTimerRange());
+	}
+
+    private void Update()
 	{
-		if(Timer1 > 0) Timer1 --;
-		if(Timer1 <= 0){
+		if(Timer > 0) Timer -= Time.deltaTime;
+		if(Timer <= 0){
 			GameObject newBee1 = Instantiate(Bee1, Point.position, transform.rotation);
-			Timer1 = MaxTimer1 * 50;
-		}
+            StartCoroutine(RandomTimerRange());
+        }
+	}
+	IEnumerator RandomTimerRange()
+	{
+        Timer = MaxTimer + Random.Range(-Scatter, Scatter);
+        yield return null;
 	}
 }
